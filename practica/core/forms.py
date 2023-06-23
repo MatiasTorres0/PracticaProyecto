addd009c799492311611927958c8b94b408eb9bd
MatiasTorres0 <mat.torres@duocuc.cl>
2023-06-23 18:17:40 -0400

    formularios modificados

--- practica/core/forms.py
+++ practica/core/forms.py
@@ -18,3 +18,28 @@ class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
         fields = ['idPaciente','nombre', 'run', 'comuna']
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    name = forms.CharField(max_length=255)
+
+    class Meta:
+        model = Usuario
+        fields = ['name']  # Asegúrate de agregar 'name' a la lista de campos
+
+class PersonaForm(forms.Form):
+    CHOICES = [
+        ('si', 'Si'),
+        ('no', 'No'),
+    ]
+
+    viudez = forms.ChoiceField(label='Viudez menor de un año', choices=CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    vivir_solo = forms.ChoiceField(label='Vivir solo', choices=CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    cambio_casa = forms.ChoiceField(label='Cambio de casa en el último año', choices=CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    no_satisfecho_residencia = forms.ChoiceField(label='No estar satisfecho con su actual lugar de residencia', choices=CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    percepcion_salud = forms.ChoiceField(label='Mala percepción de su salud, comparado con otras personas de la misma edad', choices=[(1, 'Muy mala salud'), (2, 'Mala salud'), (3, 'Normal'), (4, 'Buena salud'), (5, 'Muy buena salud')], widget=forms.Select(attrs={'class': 'form-control'}))
+    ingesta_farmacos = forms.ChoiceField(label='Ingesta diaria de 5 o más fármacos', choices=CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    limitacion_actividades = forms.ChoiceField(label='Limitación para realizar sus actividades por problemas de salud', choices=CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    problemas_salud = forms.CharField(label='Problema(s) de salud que le impida salir de la casa', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'cols': 50}))
+    hospitalizacion = forms.ChoiceField(label='Haber estado hospitalizado en el último año', choices=CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    timestamp = forms.CharField(widget=forms.HiddenInput())
--- practica/core/views.py
+++ practica/core/views.py
@@ -1,17 +1,62 @@
 from django.shortcuts import render, redirect
-from .forms import UsuarioForm, PacienteForm
+from .forms import CustomUserCreationForm, UsuarioForm, PacienteForm
 from .models import Paciente
-
+from django.contrib.auth.models import User
 def home(request):
     return render(request, 'core/home.html')
 
-def formulario(request):
+def view(request):
+    user = request.user
+    return render(request, 'index.html', {'user': user})
+
+#def formulario(request):
     nombre_buscado = "John Doe"
     personas_encontradas = Paciente.objects.filter(nombre=nombre_buscado)
     return render(request, 'core/formulario.html')
 
+def formulario(request):
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(request.POST)
+        if formulario.is_valid():
+            # Guardar los datos en la base de datos
+            formulario.save()
+
+            # Buscar el nombre en la base de datos
+            nombre = formulario.cleaned_data['nombre']
+            personas_encontradas = Paciente.objects.filter(nombre=nombre)
+
+            # Mostrar la lista de personas encontradas
+            data = {
+                'form': formulario,
+                'personas': personas_encontradas
+            }
+            return render(request, 'core/formulario.html', data)
+
+    else:
+        formulario = CustomUserCreationForm()
+
+    data = {
+        'form': formulario
+    }
+    return render(request, 'core/formulario.html', data)
+
+
+
 def prueba(request):
-    return render(request, 'core/prueba.html')
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(request.POST)
+        if formulario.is_valid():
+            # Guardar los datos en la base de datos
+            formulario.save()
+            return redirect('core/home.html')  # Reemplaza 'nombre_de_la_vista' por el nombre de la vista a la que deseas redirigir después de guardar el formulario
+    else:
+        formulario = CustomUserCreationForm()
+
+    data = {
+        'form': formulario
+    }
+    return render(request, 'core/prueba.html', data)
+
 
 def registrar_usuario(request):
     if request.method == 'POST':
